243cb813a574898b5fd78fcb1cb59f6dcec53c0f
txbounds <tmbounds@gmail.com>
2023-01-06 22:40:03 +0000

    completed the header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import  { useEffect, useState } from 'react'
 import { useLocation , useNavigate} from 'react-router-dom'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
 
 function Header() {
+
+
+const [pageState, setPageState] = useState("Sign In")  
 const location = useLocation()
 const navigate = useNavigate()
+
+const auth = getAuth()
+useEffect(()=> {
+onAuthStateChanged(auth, (user)=>{
+  if(user){
+    setPageState("Profile")
+  } else {
+    setPageState("Sign In")
+  }
+})
+}, [auth])
+
 //console.log(location.pathname)
 
 function pathMatchRoute(route) {
@@ -36,11 +52,15 @@ ${pathMatchRoute("/offers") && "text-black border-b-red-500"}
 onClick={()=> navigate("/offers")}
 >Offers</li>
 <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent
-${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}
+${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile") )&& "text-black border-b-red-500"}
 `}
 
-onClick={()=> navigate("/sign-in")}>Sign In</li>
-<li>Profile</li>
+onClick={()=> navigate("/profile")}>
+{pageState}
+
+
+</li>
+ 
 </ul>
 </div>
 
