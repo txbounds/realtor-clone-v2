422a3b928bd20e35d0b77a3a629388588bdb4e93
txbounds <tmbounds@gmail.com>
2023-01-06 21:10:36 +0000

    Added auth and logout

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from "./components/PrivateRoute";
  
 
 function App() {
@@ -23,7 +24,15 @@ function App() {
 <Route path="/offers" element={<Offers/>}></Route>
 <Route path="/sign-in" element={<SignIn/>}></Route>
 <Route path="/sign-up" element={<SignUp/>}></Route>
+
+<Route path="/profile" element={<PrivateRoute/>}>
+
 <Route path="/profile" element={<Profile/>}></Route>
+</Route>
+
+
+
+
 <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
 
 </Routes>
